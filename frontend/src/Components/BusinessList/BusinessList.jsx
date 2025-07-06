a40c72e876d25a260301f3ff9172a0a49a3b46ce
ASFAK AHMED <106955781+asfakgithub@users.noreply.github.com>
2025-07-06 17:00:53 +0530

    Update BusinessList.jsx

--- frontend/src/Components/BusinessList/BusinessList.jsx
+++ frontend/src/Components/BusinessList/BusinessList.jsx
@@ -8,7 +8,7 @@ const BusinessList = () => {
   useEffect(() => {
     const fetchBusinesses = async () => {
       try {
-        const response = await fetch('https://mini-local-business-dashboard-backend.onrender.com');
+        const response = await fetch('https://mini-local-business-dashboard-backend.onrender.com/businesses');
         if (!response.ok) {
           throw new Error('Failed to fetch businesses');
         }
